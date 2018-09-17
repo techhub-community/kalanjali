cfb785245d373bb3c5a966872f2fc4489807cd95
Abhi chaurasia <abhi.chaurasia204@gmail.com>
2018-09-17 17:33:30 +0530

    Setup basic PayTM integration

--- callback/views.py
+++ callback/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render,redirect
-
+from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
+from . import Checksum
 # Create your views here.
+@csrf_exempt
 def callback(request):
     if request.method == "POST":
-        return render(request,'callback/callback.html')
-    else:
-        return redirect('homepage')
+        MERCHANT_KEY = settings.PAYTM_MERCHANT_KEY
+        data_dict = {}
+        for key in request.POST:
+            data_dict[key] = request.POST[key]
+        verify = Checksum.verify_checksum(data_dict, MERCHANT_KEY, data_dict['CHECKSUMHASH'])
+        if verify:
+            return render(request,"callback/callback.html",{"paytm":data_dict})
+        else:
+            return HttpResponse("checksum verification failed!")
+    return HttpResponse(status=200)
--- homepage/views.py
+++ homepage/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
-
+from . import Checksum
+import uuid
+from django.conf import settings
 # Create your views here.
 
 def homepage(request):
-	return render(request,'homepage/index.html')
+	# MERCHANT_KEY = "RwH39SZTZUpQbJCY"
+	# MERCHANT_ID = "NONESt79835244795367"
+	# CALLBACK_URL ='http://206.189.133.171/callback/'
+	PAYTM_ORDER_ID = str(uuid.uuid4())
+	PAYTM_CUST_ID = str(uuid.uuid4())
+	bill_amount = 2
+	if bill_amount:
+		data_dict = {
+		'MID':settings.PAYTM_MERCHANT_ID,
+		"ORDER_ID":PAYTM_ORDER_ID,
+		"CUST_ID":PAYTM_CUST_ID,
+		'TXN_AMOUNT':str(bill_amount),
+		'INDUSTRY_TYPE_ID':settings.PAYTM_INDUSTRY_TYPE_ID,
+		'WEBSITE':settings.PAYTM_WEBSITE,
+		'CHANNEL_ID':settings.PAYTM_CHANNEL_ID,
+		'CALLBACK_URL':settings.PAYTM_CALLBACK_URL,
+		}
+		param_dict = data_dict
+		param_dict['CHECKSUMHASH'] = Checksum.generate_checksum(data_dict, settings.PAYTM_MERCHANT_KEY)
+		return render(request,"homepage/index.html",{'paytmdict':param_dict})
+	else:
+		return HttpResponse("Bill Amount Could not find. ?bill_amount=2")
