3e98663532ae3e3b2743031550bfc7b8d13fe39a
Abhi chaurasia <abhi.chaurasia204@gmail.com>
2018-09-24 14:36:05 +0530

    update register view

--- homepage/forms.py
+++ homepage/forms.py
@@ -8,12 +8,10 @@ class RegistrationForm(forms.Form):
         ("E3","Event 3"),
         )
 
-    first_name = forms.CharField(label="First Name",max_length=20)
-    last_name = forms.CharField(label="Last Name",max_length=20)
+    name = forms.CharField(max_length=20)
     phone = forms.IntegerField(min_value=1000000000,max_value=9999999999,widget=forms.widgets.TextInput)
     email = forms.EmailField()
     college = forms.CharField(max_length=50)
-    branch = forms.CharField(max_length=50)
-    semester = forms.IntegerField(min_value=1,max_value=8)
+    year = forms.IntegerField(min_value=1,max_value=4)
     event_selected = forms.ChoiceField(choices=choice_list)
     txn_id = forms.CharField(label="Transaction ID")
--- homepage/urls.py
+++ homepage/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.msg_api,name='msg_api'),
+    path('',views.register,name='register'),
 ]
